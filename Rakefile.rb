ee18113b6aa18ed7a98870c63ff4214f02f5387a
presentation4u2016 <presentation4u2016@gmail.com>
2016-06-02 11:17:40 +0000

    Add files via upload

---
+++ Rakefile.rb
@@ -0,0 +1,15 @@
+require 'rubygems'
+require 'rake'
+require 'rspec/core/rake_task'
+
+
+############# ALL ############# 
+
+desc "Run all tests"
+# RUN ALL
+RSpec::Core::RakeTask.new('all') do |t|
+  t.rspec_opts = ["--format documentation","--color"]
+  t.pattern = [
+  	'LivingSocial/*.rb'
+  ]
+end 
